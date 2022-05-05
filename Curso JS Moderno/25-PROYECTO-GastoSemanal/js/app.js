9ba102f4ed6d2844c66529aa711e1e32646be738
jramon901225 <jramon901225@gmail.com>
2022-05-05 16:48:11 -0500

    litsdor de gastos

--- Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
+++ Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -6,6 +6,8 @@ const listaGastos = document.querySelector("#gastos ul");
 eventListeners();
 function eventListeners() {
     document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+
+    formulario.addEventListener("submit", agregarGasto);
 }
 
 //Clases
@@ -15,6 +17,10 @@ class Presupuesto {
         this.restante = Number(presupuesto);
         this.gastos = [];
     }
+
+    agregarGasto(gasto) {
+        this.gastos = [...this.gastos, gasto];
+    }
 }
 
 class UI {
@@ -22,6 +28,31 @@ class UI {
         const { presupuesto, restante } = cantidad;
         document.querySelector("#total").textContent = presupuesto;
         document.querySelector("#restante").textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo) {
+        //Crear div para plasmar el mensaje
+        const divAlerta = document.createElement("div");
+        divAlerta.classList.add("text-center", "alert");
+        //Verificamos el tipo de mensaje
+        if (tipo === "error") {
+            divAlerta.classList.add("alert-danger");
+        } else {
+            divAlerta.classList.add("alert-success");
+        }
+        //Agregamos el texto al mensaje
+        divAlerta.textContent = mensaje;
+        //Insertamos en el HTML
+        document.querySelector(".primario").insertBefore(divAlerta, formulario);
+
+        //Quitar el mensaje
+        setTimeout(() => {
+            divAlerta.remove();
+        }, 3000);
+    }
+
+    agregarGastoListado(gasto) {
+        console.log(gasto);
     }
 }
 
@@ -43,7 +74,35 @@ function preguntarPresupuesto() {
 
     //Presupuesto valido
     presupuesto = new Presupuesto(presupuestoUsuario);
-    console.log(presupuesto);
 
     ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+    const nombreGasto = document.querySelector("#gasto").value;
+    const cantidad = Number(document.querySelector("#cantidad").value);
+
+    if (nombreGasto === "" || cantidad === "") {
+        ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+        return;
+    } else if (cantidad <= 0 || isNaN(cantidad)) {
+        ui.imprimirAlerta("La cantidad ingresada no es correcta", "error");
+        return;
+    } else {
+        ui.imprimirAlerta("Gasto agregado con exito", "success");
+    }
+
+    const gasto = {
+        nombreGasto,
+        cantidad,
+        id: Date.now(),
+    };
+
+    presupuesto.agregarGasto(gasto);
+
+    const { gastos } = presupuesto;
+
+    ui.agregarGastoListado(gastos);
+
+    formulario.reset();
 }
