41b5c6be9d23504b95e9b9aefff2d07c7710b375
jramon901225 <jramon901225@gmail.com>
2022-05-09 12:42:37 -0500

    Expenses quoter finished

--- Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
+++ Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -29,7 +29,11 @@ class Presupuesto {
             0
         );
         this.restante = this.presupuesto - gastado;
-        console.log(this.restante);
+    }
+
+    eliminarGasto(id) {
+        this.gastos = this.gastos.filter((gasto) => gasto.id !== id);
+        this.calcularRestante();
     }
 }
 
@@ -83,6 +87,9 @@ class UI {
             const btnBorrar = document.createElement("button");
             btnBorrar.textContent = "Eliminar";
             btnBorrar.classList.add("btn", "btn-danger", "borrar-gasto");
+            btnBorrar.onclick = () => {
+                eliminarGasto(id);
+            };
 
             //Agregar boton para borrar el gasto
             nuevoGasto.appendChild(btnBorrar);
@@ -101,9 +108,32 @@ class UI {
     actualizarRestante(restante) {
         document.querySelector("#restante").textContent = restante;
     }
+
+    comprobarPresupuesto(presupuestoObj) {
+        const { presupuesto, restante } = presupuestoObj;
+        const restanteDiv = document.querySelector(".restante");
+
+        //Comprobar el gasto de 25%
+        if (presupuesto / 4 > restante) {
+            restanteDiv.classList.remove("alert-success", "alert-warning");
+            restanteDiv.classList.add("alert-danger");
+        } else if (presupuesto / 2 > restante) {
+            restanteDiv.classList.remove("alert-success");
+            restanteDiv.classList.add("alert-warning");
+        } else {
+            restanteDiv.classList.remove("alert-danger", "alert-warning");
+            restanteDiv.classList.add("alert-success");
+        }
+
+        //Si el total es cero o menor
+        if (restante <= 0) {
+            ui.imprimirAlerta("el presupuesto ya se ha agotado", "error");
+            formulario.querySelector('button[type="submit"]').disabled = true;
+        }
+    }
 }
 
-//Instanciar
+//Instanciamos las clases
 const ui = new UI();
 let presupuesto;
 
@@ -153,5 +183,19 @@ function agregarGasto(e) {
 
     ui.actualizarRestante(restante);
 
+    ui.comprobarPresupuesto(presupuesto);
+
     formulario.reset();
+}
+
+function eliminarGasto(id) {
+    presupuesto.eliminarGasto(id);
+
+    //Elimina los gastos del HTML
+    const { gastos, restante } = presupuesto;
+    ui.agregarGastoListado(gastos);
+
+    ui.actualizarRestante(restante);
+
+    ui.comprobarPresupuesto(presupuesto);
 }
